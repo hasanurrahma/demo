52f8be3ad1e10856a3520f3345644d82914372ad
Hasanur Rahman <105588130+hasanur432@users.noreply.github.com>
2024-05-28 17:32:50 +0530

    check

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,38 +1,12 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-
-// const Home: React.FC = () => {
-//   return (
-//     <div
-//       style={{
-//         display: "flex",
-//         justifyContent: "center",
-//         alignItems: "center",
-//         height: "100vh",
-//       }}
-//     >
-//       <div style={{ textAlign: "center" }}>
-//         <Link to="/registration">
-//           <button>Registration</button>
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Home;
-
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Home: React.FC = () => {
   const [data, setData] = useState<any[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // Fetch data from your API endpoint
         const response = await fetch(
           `https://demobackend-jiv0.onrender.com/images`
         );
@@ -44,8 +18,6 @@ const Home: React.FC = () => {
         }
       } catch (error) {
         console.error("Error fetching data:", error);
-      } finally {
-        setLoading(false);
       }
     };
 
@@ -65,31 +37,25 @@ const Home: React.FC = () => {
     >
       <div style={{ textAlign: "center" }}>
         <h1>Home Page</h1>
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
-          <>
-            <table style={{ margin: "auto" }}>
-              <thead>
-                <tr>
-                  <th>Make And Model</th>
-                  <th>Rate Card Segment</th>
-                </tr>
-              </thead>
-              <tbody>
-                {data.map((item, index) => (
-                  <tr key={index}>
-                    <td>{item.makeAndMod}</td>
-                    <td>{item.rateCardSegment}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-            <Link to="/registration">
-              <button>Registration</button>
-            </Link>
-          </>
-        )}
+        <table style={{ margin: "auto" }}>
+          <thead>
+            <tr>
+              <th>Make And Model</th>
+              <th>Rate Card Segment</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index}>
+                <td>{item.makeAndMod}</td>
+                <td>{item.rateCardSegment}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <Link to="/registration">
+          <button>Registration</button>
+        </Link>
       </div>
     </div>
   );
